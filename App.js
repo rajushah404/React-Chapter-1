78fb0c0178e09b7f08e7ba3feb12e902bee3012e
rajushah404 <rajushah2286@gmail.com>
2025-02-04 12:20:00 +0545

    Parcel added for bundeling

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am React"),
-    React.createElement("h2", {}, "I am React H2"),
+    React.createElement("h1", {}, "Hello Worls"),
+    React.createElement("h2", {}, "Hey"),
   ]),
   React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I am React"),
-    React.createElement("h2", {}, "I am React H2"),
+    React.createElement("h1", {}, "Hello React"),
+    React.createElement("h2", {}, "Love React"),
   ]),
 ]);
 
