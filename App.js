4409d732cecd053efae864a969865a0ad6e9138a
rajushah404 <rajushah2286@gmail.com>
2025-02-13 20:44:58 +0545

    CSS added

--- App.js
+++ App.js
@@ -30,38 +30,259 @@ const Header = () => {
 };
 
 const RestroCard = (props) => {
+  const { resData } = props;
+  const {image,name,aggregateRating,address} = resData?.item;
   return (
     <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
-      <img className="res-logo" src={props.logo} alt={props.alt + "logo"} />
-      <h3>{props.title}</h3>
-      <h4>{props.cusine}</h4>
-      <h4>{props.rating}</h4>
-      <h4>{props.delivaryTime}</h4>
+      <img className="res-logo" src={image} alt="logo" />
+      <h3>{name}</h3>
+      <h4>Rating: {aggregateRating.ratingValue} ⭐</h4>
+      <h4>Reviews: {aggregateRating.reviewCount}</h4>
+      <h4>Address: {address.streetAddress}</h4>
     </div>
   );
 };
+
+const resList = [
+  {
+    "@type": "ListItem",
+    position: 1,
+    item: {
+      "@type": "Restaurant",
+      name: "Yummy Foods",
+      image:
+        "https://b.zmtcdn.com/data/pictures/2/19212482/5378ac76f96d77823576c775c2375a96_o2_featured_v2.jpg?output-format=webp",
+      aggregateRating: {
+        "@type": "AggregateRating",
+        ratingValue: "3.6",
+        reviewCount: "11.4K",
+      },
+      url: "/motihari/yummy-foods-motihari-locality/order",
+      address: {
+        "@type": "PostalAddress",
+        addressCountry: "",
+        addressLocality: "",
+        addressRegion: "",
+        postalCode: "",
+        streetAddress:
+          "3rd floor, Above Bazar India, Main Road, Motihari Sadar, Purba Champaran(Bihar), Motihari Locality, Motihari",
+      },
+      servesCuisine: "Biryani, Chinese, Fast Food",
+    },
+  },
+  {
+    "@type": "ListItem",
+    position: 2,
+    item: {
+      "@type": "Restaurant",
+      name: "Craft Momo",
+      image:
+        "https://b.zmtcdn.com/data/pictures/8/20778288/4951f967eef5e8fef9d94532e7552807_o2_featured_v2.jpg?output-format=webp",
+      aggregateRating: {
+        "@type": "AggregateRating",
+        ratingValue: "4.2",
+        reviewCount: "1,626",
+      },
+      url: "/motihari/craft-momo-motihari-locality/order",
+      address: {
+        "@type": "PostalAddress",
+        addressCountry: "",
+        addressLocality: "",
+        addressRegion: "",
+        postalCode: "",
+        streetAddress: "Madhuban Chawni Chowk, Motihari Locality, Motihari",
+      },
+      servesCuisine: "Momos",
+    },
+  },
+  {
+    "@type": "ListItem",
+    position: 3,
+    item: {
+      "@type": "Restaurant",
+      name: "KFC",
+      image:
+        "https://b.zmtcdn.com/data/pictures/8/19924368/a32f5869b6d9b47c9fda4af9745fd3c9_o2_featured_v2.jpg?output-format=webp",
+      aggregateRating: {
+        "@type": "AggregateRating",
+        ratingValue: "4.6",
+        reviewCount: "3,758",
+      },
+      url: "/motihari/kfc-motihari-locality/order",
+      address: {
+        "@type": "PostalAddress",
+        addressCountry: "",
+        addressLocality: "",
+        addressRegion: "",
+        postalCode: "",
+        streetAddress:
+          "Ground Floor, Khata 2, Ward 20, Bhawanipur Zirat, Arya Samaj Road, Motihari Locality, Motihari",
+      },
+      servesCuisine: "Burger, Fast Food, Rolls",
+    },
+  },
+  {
+    "@type": "ListItem",
+    position: 4,
+    item: {
+      "@type": "Restaurant",
+      name: "Suman Hotel",
+      image:
+        "https://b.zmtcdn.com/data/pictures/2/19093482/8dafcf32ae6e9013068581a59d04e287_o2_featured_v2.jpg?output-format=webp",
+      aggregateRating: {
+        "@type": "AggregateRating",
+        ratingValue: "3.4",
+        reviewCount: "1,169",
+      },
+      url: "/motihari/suman-hotel-motihari-locality/order",
+      address: {
+        "@type": "PostalAddress",
+        addressCountry: "",
+        addressLocality: "",
+        addressRegion: "",
+        postalCode: "",
+        streetAddress: "Chatauni Chowk Motihari, Motihari Locality, Motihari",
+      },
+      servesCuisine: "North Indian",
+    },
+  },
+  {
+    "@type": "ListItem",
+    position: 5,
+    item: {
+      "@type": "Restaurant",
+      name: "Chef Vishal Hotel And Restaurant",
+      image:
+        "https://b.zmtcdn.com/data/pictures/9/20411669/37750871991fc4a1dfb8c9013cab2498_o2_featured_v2.jpg?output-format=webp",
+      aggregateRating: {
+        "@type": "AggregateRating",
+        ratingValue: "3.6",
+        reviewCount: "1,598",
+      },
+      url: "/motihari/chef-vishal-hotel-and-restaurant-motihari-locality/order",
+      address: {
+        "@type": "PostalAddress",
+        addressCountry: "",
+        addressLocality: "",
+        addressRegion: "",
+        postalCode: "",
+        streetAddress:
+          "Jhanpull Chowk, Arya Nagar, Station Road, Motihari Locality, Motihari",
+      },
+      servesCuisine: "North Indian, South Indian, Chinese",
+    },
+  },
+  {
+    "@type": "ListItem",
+    position: 6,
+    item: {
+      "@type": "Restaurant",
+      name: "JVW Arena",
+      image:
+        "https://b.zmtcdn.com/data/pictures/9/21037529/24c9c0c6752421848311c26de932f65f_o2_featured_v2.jpg?output-format=webp",
+      aggregateRating: {
+        "@type": "AggregateRating",
+        ratingValue: "4.1",
+        reviewCount: "275",
+      },
+      url: "/motihari/jvw-arena-motihari-locality/order",
+      address: {
+        "@type": "PostalAddress",
+        addressCountry: "",
+        addressLocality: "",
+        addressRegion: "",
+        postalCode: "",
+        streetAddress:
+          "NH 28A, Mathiya Zirat, Near JJ Kanak Hospital, Motihari Locality, Motihari",
+      },
+      servesCuisine: "North Indian, Biryani, Chinese",
+    },
+  },
+  {
+    "@type": "ListItem",
+    position: 7,
+    item: {
+      "@type": "Restaurant",
+      name: "11th Avenue",
+      image:
+        "https://b.zmtcdn.com/data/pictures/1/20312171/3dfba4890e6d5f3709e14e6eed07f337_o2_featured_v2.jpg?output-format=webp",
+      aggregateRating: {
+        "@type": "AggregateRating",
+        ratingValue: "4.1",
+        reviewCount: "430",
+      },
+      url: "/motihari/11th-avenue-motihari-locality/order",
+      address: {
+        "@type": "PostalAddress",
+        addressCountry: "",
+        addressLocality: "",
+        addressRegion: "",
+        postalCode: "",
+        streetAddress:
+          "Chandmari, Near Ms Collage, Motihari Locality, Motihari",
+      },
+      servesCuisine: "Chinese, North Indian, Mughlai",
+    },
+  },
+  {
+    "@type": "ListItem",
+    position: 8,
+    item: {
+      "@type": "Restaurant",
+      name: "Moti Mahal Tandoori Trail",
+      image:
+        "https://b.zmtcdn.com/data/pictures/2/21506142/2b40353c49b4afdb7d22bb72de9794a6_o2_featured_v2.jpg?output-format=webp",
+      aggregateRating: {
+        "@type": "AggregateRating",
+        ratingValue: "3.9",
+        reviewCount: "69",
+      },
+      url: "/motihari/moti-mahal-tandoori-trail-motihari-locality/order",
+      address: {
+        "@type": "PostalAddress",
+        addressCountry: "",
+        addressLocality: "",
+        addressRegion: "",
+        postalCode: "",
+        streetAddress:
+          "Second Floor, RJ Plaza, Janpul Chowk, East Champaran, Motihari Locality, Motihari",
+      },
+      servesCuisine: "North Indian, Chinese, Biryani",
+    },
+  },
+  {
+    "@type": "ListItem",
+    position: 9,
+    item: {
+      "@type": "Restaurant",
+      name: "Foodie Moodie",
+      image:
+        "https://b.zmtcdn.com/data/pictures/7/21603417/c16b5325673d9fab822e7717d1746857_o2_featured_v2.jpg?output-format=webp",
+      aggregateRating: {
+        "@type": "AggregateRating",
+        ratingValue: "4.2",
+        reviewCount: "50",
+      },
+      url: "/motihari/foodie-moodie-3-motihari-locality/order",
+      address: {
+        "@type": "PostalAddress",
+        addressCountry: "",
+        addressLocality: "",
+        addressRegion: "",
+        postalCode: "",
+        streetAddress: "Bangali Colony, Gali 5, Motihari Locality, Motihari",
+      },
+      servesCuisine: "North Indian, Sandwich, Burger",
+    },
+  },
+];
 
 const Body = () => {
   return (
     <div className="body">
       <div className="search">Search</div>
       <div className="res-container">
-        <RestroCard
-          title="Thakali"
-          cusine="Nepali Khana"
-          rating="9*"
-          delivaryTime="30 min"
-          logo="https://cdn.pixabay.com/photo/2016/03/05/19/02/hamburger-1238246_1280.jpg"
-          alt="thakali"
-        />
-                <RestroCard
-          title="Indian"
-          cusine="Indian Khana"
-          rating="9*"
-          delivaryTime="30 min"
-          logo="https://cdn.pixabay.com/photo/2016/03/05/19/02/hamburger-1238246_1280.jpg"
-          alt="indian"
-        />
+        {resList.map(res => <RestroCard resData = {res}/>)}
       </div>
     </div>
   );
@@ -75,6 +296,8 @@ const AppLayout = () => {
     </div>
   );
 };
+
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
--- dist/index.7826abd7.js
+++ dist/index.7826abd7.js
@@ -3027,6 +3027,8 @@ var _clientDefault = parcelHelpers.interopDefault(_client);
 };
 _c = Header;
 const RestroCard = (props)=>{
+    const { resData } = props;
+    const { image, name, aggregateRating, address } = resData?.item;
     return /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("div", {
         className: "res-card",
         style: {
@@ -3035,49 +3037,277 @@ const RestroCard = (props)=>{
         children: [
             /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("img", {
                 className: "res-logo",
-                src: props.logo,
-                alt: props.alt + "logo"
-            }, void 0, false, {
-                fileName: "App.js",
-                lineNumber: 35,
-                columnNumber: 7
-            }, undefined),
-            /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("h3", {
-                children: props.title
-            }, void 0, false, {
-                fileName: "App.js",
-                lineNumber: 36,
-                columnNumber: 7
-            }, undefined),
-            /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("h4", {
-                children: props.cusine
+                src: image,
+                alt: "logo"
             }, void 0, false, {
                 fileName: "App.js",
                 lineNumber: 37,
                 columnNumber: 7
             }, undefined),
-            /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("h4", {
-                children: props.rating
+            /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("h3", {
+                children: name
             }, void 0, false, {
                 fileName: "App.js",
                 lineNumber: 38,
                 columnNumber: 7
             }, undefined),
             /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("h4", {
-                children: props.delivaryTime
-            }, void 0, false, {
+                children: [
+                    "Rating: ",
+                    aggregateRating.ratingValue,
+                    " \u2B50"
+                ]
+            }, void 0, true, {
                 fileName: "App.js",
                 lineNumber: 39,
+                columnNumber: 7
+            }, undefined),
+            /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("h4", {
+                children: [
+                    "Reviews: ",
+                    aggregateRating.reviewCount
+                ]
+            }, void 0, true, {
+                fileName: "App.js",
+                lineNumber: 40,
+                columnNumber: 7
+            }, undefined),
+            /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("h4", {
+                children: [
+                    "Address: ",
+                    address.streetAddress
+                ]
+            }, void 0, true, {
+                fileName: "App.js",
+                lineNumber: 41,
                 columnNumber: 7
             }, undefined)
         ]
     }, void 0, true, {
         fileName: "App.js",
-        lineNumber: 34,
+        lineNumber: 36,
         columnNumber: 5
     }, undefined);
 };
 _c1 = RestroCard;
+const resList = [
+    {
+        "@type": "ListItem",
+        position: 1,
+        item: {
+            "@type": "Restaurant",
+            name: "Yummy Foods",
+            image: "https://b.zmtcdn.com/data/pictures/2/19212482/5378ac76f96d77823576c775c2375a96_o2_featured_v2.jpg?output-format=webp",
+            aggregateRating: {
+                "@type": "AggregateRating",
+                ratingValue: "3.6",
+                reviewCount: "11.4K"
+            },
+            url: "/motihari/yummy-foods-motihari-locality/order",
+            address: {
+                "@type": "PostalAddress",
+                addressCountry: "",
+                addressLocality: "",
+                addressRegion: "",
+                postalCode: "",
+                streetAddress: "3rd floor, Above Bazar India, Main Road, Motihari Sadar, Purba Champaran(Bihar), Motihari Locality, Motihari"
+            },
+            servesCuisine: "Biryani, Chinese, Fast Food"
+        }
+    },
+    {
+        "@type": "ListItem",
+        position: 2,
+        item: {
+            "@type": "Restaurant",
+            name: "Craft Momo",
+            image: "https://b.zmtcdn.com/data/pictures/8/20778288/4951f967eef5e8fef9d94532e7552807_o2_featured_v2.jpg?output-format=webp",
+            aggregateRating: {
+                "@type": "AggregateRating",
+                ratingValue: "4.2",
+                reviewCount: "1,626"
+            },
+            url: "/motihari/craft-momo-motihari-locality/order",
+            address: {
+                "@type": "PostalAddress",
+                addressCountry: "",
+                addressLocality: "",
+                addressRegion: "",
+                postalCode: "",
+                streetAddress: "Madhuban Chawni Chowk, Motihari Locality, Motihari"
+            },
+            servesCuisine: "Momos"
+        }
+    },
+    {
+        "@type": "ListItem",
+        position: 3,
+        item: {
+            "@type": "Restaurant",
+            name: "KFC",
+            image: "https://b.zmtcdn.com/data/pictures/8/19924368/a32f5869b6d9b47c9fda4af9745fd3c9_o2_featured_v2.jpg?output-format=webp",
+            aggregateRating: {
+                "@type": "AggregateRating",
+                ratingValue: "4.6",
+                reviewCount: "3,758"
+            },
+            url: "/motihari/kfc-motihari-locality/order",
+            address: {
+                "@type": "PostalAddress",
+                addressCountry: "",
+                addressLocality: "",
+                addressRegion: "",
+                postalCode: "",
+                streetAddress: "Ground Floor, Khata 2, Ward 20, Bhawanipur Zirat, Arya Samaj Road, Motihari Locality, Motihari"
+            },
+            servesCuisine: "Burger, Fast Food, Rolls"
+        }
+    },
+    {
+        "@type": "ListItem",
+        position: 4,
+        item: {
+            "@type": "Restaurant",
+            name: "Suman Hotel",
+            image: "https://b.zmtcdn.com/data/pictures/2/19093482/8dafcf32ae6e9013068581a59d04e287_o2_featured_v2.jpg?output-format=webp",
+            aggregateRating: {
+                "@type": "AggregateRating",
+                ratingValue: "3.4",
+                reviewCount: "1,169"
+            },
+            url: "/motihari/suman-hotel-motihari-locality/order",
+            address: {
+                "@type": "PostalAddress",
+                addressCountry: "",
+                addressLocality: "",
+                addressRegion: "",
+                postalCode: "",
+                streetAddress: "Chatauni Chowk Motihari, Motihari Locality, Motihari"
+            },
+            servesCuisine: "North Indian"
+        }
+    },
+    {
+        "@type": "ListItem",
+        position: 5,
+        item: {
+            "@type": "Restaurant",
+            name: "Chef Vishal Hotel And Restaurant",
+            image: "https://b.zmtcdn.com/data/pictures/9/20411669/37750871991fc4a1dfb8c9013cab2498_o2_featured_v2.jpg?output-format=webp",
+            aggregateRating: {
+                "@type": "AggregateRating",
+                ratingValue: "3.6",
+                reviewCount: "1,598"
+            },
+            url: "/motihari/chef-vishal-hotel-and-restaurant-motihari-locality/order",
+            address: {
+                "@type": "PostalAddress",
+                addressCountry: "",
+                addressLocality: "",
+                addressRegion: "",
+                postalCode: "",
+                streetAddress: "Jhanpull Chowk, Arya Nagar, Station Road, Motihari Locality, Motihari"
+            },
+            servesCuisine: "North Indian, South Indian, Chinese"
+        }
+    },
+    {
+        "@type": "ListItem",
+        position: 6,
+        item: {
+            "@type": "Restaurant",
+            name: "JVW Arena",
+            image: "https://b.zmtcdn.com/data/pictures/9/21037529/24c9c0c6752421848311c26de932f65f_o2_featured_v2.jpg?output-format=webp",
+            aggregateRating: {
+                "@type": "AggregateRating",
+                ratingValue: "4.1",
+                reviewCount: "275"
+            },
+            url: "/motihari/jvw-arena-motihari-locality/order",
+            address: {
+                "@type": "PostalAddress",
+                addressCountry: "",
+                addressLocality: "",
+                addressRegion: "",
+                postalCode: "",
+                streetAddress: "NH 28A, Mathiya Zirat, Near JJ Kanak Hospital, Motihari Locality, Motihari"
+            },
+            servesCuisine: "North Indian, Biryani, Chinese"
+        }
+    },
+    {
+        "@type": "ListItem",
+        position: 7,
+        item: {
+            "@type": "Restaurant",
+            name: "11th Avenue",
+            image: "https://b.zmtcdn.com/data/pictures/1/20312171/3dfba4890e6d5f3709e14e6eed07f337_o2_featured_v2.jpg?output-format=webp",
+            aggregateRating: {
+                "@type": "AggregateRating",
+                ratingValue: "4.1",
+                reviewCount: "430"
+            },
+            url: "/motihari/11th-avenue-motihari-locality/order",
+            address: {
+                "@type": "PostalAddress",
+                addressCountry: "",
+                addressLocality: "",
+                addressRegion: "",
+                postalCode: "",
+                streetAddress: "Chandmari, Near Ms Collage, Motihari Locality, Motihari"
+            },
+            servesCuisine: "Chinese, North Indian, Mughlai"
+        }
+    },
+    {
+        "@type": "ListItem",
+        position: 8,
+        item: {
+            "@type": "Restaurant",
+            name: "Moti Mahal Tandoori Trail",
+            image: "https://b.zmtcdn.com/data/pictures/2/21506142/2b40353c49b4afdb7d22bb72de9794a6_o2_featured_v2.jpg?output-format=webp",
+            aggregateRating: {
+                "@type": "AggregateRating",
+                ratingValue: "3.9",
+                reviewCount: "69"
+            },
+            url: "/motihari/moti-mahal-tandoori-trail-motihari-locality/order",
+            address: {
+                "@type": "PostalAddress",
+                addressCountry: "",
+                addressLocality: "",
+                addressRegion: "",
+                postalCode: "",
+                streetAddress: "Second Floor, RJ Plaza, Janpul Chowk, East Champaran, Motihari Locality, Motihari"
+            },
+            servesCuisine: "North Indian, Chinese, Biryani"
+        }
+    },
+    {
+        "@type": "ListItem",
+        position: 9,
+        item: {
+            "@type": "Restaurant",
+            name: "Foodie Moodie",
+            image: "https://b.zmtcdn.com/data/pictures/7/21603417/c16b5325673d9fab822e7717d1746857_o2_featured_v2.jpg?output-format=webp",
+            aggregateRating: {
+                "@type": "AggregateRating",
+                ratingValue: "4.2",
+                reviewCount: "50"
+            },
+            url: "/motihari/foodie-moodie-3-motihari-locality/order",
+            address: {
+                "@type": "PostalAddress",
+                addressCountry: "",
+                addressLocality: "",
+                addressRegion: "",
+                postalCode: "",
+                streetAddress: "Bangali Colony, Gali 5, Motihari Locality, Motihari"
+            },
+            servesCuisine: "North Indian, Sandwich, Burger"
+        }
+    }
+];
 const Body = ()=>{
     return /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("div", {
         className: "body",
@@ -3087,46 +3317,27 @@ const Body = ()=>{
                 children: "Search"
             }, void 0, false, {
                 fileName: "App.js",
-                lineNumber: 47,
+                lineNumber: 283,
                 columnNumber: 7
             }, undefined),
             /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("div", {
                 className: "res-container",
-                children: [
-                    /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)(RestroCard, {
-                        title: "Thakali",
-                        cusine: "Nepali Khana",
-                        rating: "9*",
-                        delivaryTime: "30 min",
-                        logo: "https://cdn.pixabay.com/photo/2016/03/05/19/02/hamburger-1238246_1280.jpg",
-                        alt: "thakali"
+                children: resList.map((res)=>/*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)(RestroCard, {
+                        resData: res
                     }, void 0, false, {
                         fileName: "App.js",
-                        lineNumber: 49,
-                        columnNumber: 9
-                    }, undefined),
-                    /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)(RestroCard, {
-                        title: "Indian",
-                        cusine: "Indian Khana",
-                        rating: "9*",
-                        delivaryTime: "30 min",
-                        logo: "https://cdn.pixabay.com/photo/2016/03/05/19/02/hamburger-1238246_1280.jpg",
-                        alt: "indian"
-                    }, void 0, false, {
-                        fileName: "App.js",
-                        lineNumber: 57,
-                        columnNumber: 17
-                    }, undefined)
-                ]
-            }, void 0, true, {
+                        lineNumber: 285,
+                        columnNumber: 29
+                    }, undefined))
+            }, void 0, false, {
                 fileName: "App.js",
-                lineNumber: 48,
+                lineNumber: 284,
                 columnNumber: 7
             }, undefined)
         ]
     }, void 0, true, {
         fileName: "App.js",
-        lineNumber: 46,
+        lineNumber: 282,
         columnNumber: 5
     }, undefined);
 };
@@ -3137,18 +3348,18 @@ const AppLayout = ()=>{
         children: [
             /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)(Header, {}, void 0, false, {
                 fileName: "App.js",
-                lineNumber: 73,
+                lineNumber: 294,
                 columnNumber: 7
             }, undefined),
             /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)(Body, {}, void 0, false, {
                 fileName: "App.js",
-                lineNumber: 74,
+                lineNumber: 295,
                 columnNumber: 7
             }, undefined)
         ]
     }, void 0, true, {
         fileName: "App.js",
-        lineNumber: 72,
+        lineNumber: 293,
         columnNumber: 5
     }, undefined);
 };
@@ -3156,7 +3367,7 @@ _c3 = AppLayout;
 const root = (0, _clientDefault.default).createRoot(document.getElementById("root"));
 root.render(/*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)(AppLayout, {}, void 0, false, {
     fileName: "App.js",
-    lineNumber: 81,
+    lineNumber: 304,
     columnNumber: 13
 }, undefined));
 var _c, _c1, _c2, _c3;
