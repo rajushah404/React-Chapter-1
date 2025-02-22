f3940f07a132601aa2845f51c374058071292e3d
rajushah404 <rajushah2286@gmail.com>
2025-02-22 23:15:09 +0545

    search bar added

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,40 @@
 import RestroCard from "./RestroCard";
 import resList from "../utils/mockData";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Body = () => {
   // Local State Variable.
   const [listOfRestros, setListOfRestros] = useState(resList);
+  const [searchQuery, setSearchQuery] = useState("");
+
+  // const filterSearchRestro = () => {
+  //   const searchList = resList.filter((res) =>
+  //     res.item.name.toLowerCase().includes(searchQuery.toLowerCase())
+  //   );
+  //   setListOfRestros(searchList);
+  // };
+
+  useEffect(() => {
+    const searchList = resList.filter((res) =>
+      res.item.name.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+    setListOfRestros(searchList);
+  }, [searchQuery]);
 
   return (
     <div className="body">
       <div className="filter">
+        <input
+          id="search"
+          name="search"
+          type="text"
+          placeholder="Search for restaurants..."
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        {/* <button className="filter-btn" onClick={filterSearchRestro}>
+          Search
+        </button> */}
         <button
           className="filter-btn"
           onClick={() => {
